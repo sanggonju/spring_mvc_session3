8e76e9345509cb34caa8572f38b7aa0bae956b8b
goOn <terusg90@gmail.com>
2020-06-01 15:46:03 +0900

    first commit

---
+++ src/main/java/me/gon/demowebmvcsession3/DemoWebMvcSession3Application.java
@@ -0,0 +1,13 @@
+package me.gon.demowebmvcsession3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebMvcSession3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebMvcSession3Application.class, args);
+    }
+
+}
